a27906f594bd74b4652b31a06dd18e0e1a106245
Gott50 <xxGott50xx@gmail.com>
2017-03-01 17:01:28 +0100

    add Button

--- src/WebViewer.jsx
+++ src/WebViewer.jsx
@@ -4,10 +4,18 @@ import config from "./config";
 import "./WebViewer.css";
 
 class WebViewer extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            mIndex: 0
+        }
+    }
+
     render() {
         return (
-            <div>
-                { this.getWorkout() }
+            <div className="WebViewer">
+                { this.getWorkout(0,this.state.mIndex) }
+                <button onClick={this.handleClick.bind(this)}>Got it!</button>
             </div>
         );
     }
@@ -21,25 +29,34 @@ class WebViewer extends Component {
         return <img src={imageUrl} role="presentation"/>;
     }
 
-    getWorkout(index) {
-        index = index || 0;
-        return config[index].messages.map((message)=>{
-            switch (message.type) {
-                case 0:
-                    return this.addText(message.speech);
-                case -1:
-                    return this.addClock(message.duration);
-                case 3:
-                    return this.addImage(message.imageUrl);
-                default:
-                    return this.addText("unknown Type: " + message.type);
-            }
-        });
+    getWorkout(cIndex, mIndex) {
+        mIndex = mIndex || 0;
+        cIndex = cIndex || 0;
+        return this.mapWorkout(config[cIndex].messages[mIndex]);
+    }
+
+    mapWorkout(message) {
+        switch (message.type) {
+            case 0:
+                return this.addText(message.speech);
+            case -1:
+                return this.addClock(message.duration);
+            case 3:
+                return this.addImage(message.imageUrl);
+            default:
+                return this.addText("unknown Type: " + message.type);
+        }
     }
 
     addClock(duration) {
         duration = duration || 5;
         return <Clock duration={duration}/>
+    }
+
+    handleClick() {
+        this.setState({
+           mIndex: this.state.mIndex + 1
+        });
     }
 }
 
