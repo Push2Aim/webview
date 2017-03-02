4c6902c6d010e651b3948228fc3b6d173c96f8a9
Gott50 <xxGott50xx@gmail.com>
2017-03-02 12:22:44 +0100

    add ViewTrophy CSS

--- src/ViewTrophy.jsx
+++ src/ViewTrophy.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from "react";
-// import "./ViewText.css";
+import "./ViewTrophy.css";
 
 class ViewTrophy extends Component {
     render() {
         return (
             <div className="ViewTrophy">
                 <img src={this.props.image} role="presentation"/>
-                <p>{this.props.text}</p>
+                <p className="intro_rectangle">{this.props.text}</p>
             </div>
         );
     }
