58f78e9dcb38b10eba2956743089e69c42e72860
Gott50 <xxGott50xx@gmail.com>
2017-03-17 19:32:10 +0100

    add config of Welcome, Warmup, Stretching, AMRAPtut, AMRAP

--- src/config.js
+++ src/config.js
@@ -6,11 +6,95 @@ const config = [
             {
                 "type": "Trophy",
                 "title": "Congratulations",
-                "trophy_pic": "http://uploads.webflow.com/58c062e37f6bd43c050cce7f/58c352892387178e61460262_Trophy.png",
-                "text": {
+                "images": {
+                    "trophy_pic": "http://uploads.webflow.com/58c062e37f6bd43c050cce7f/58c352892387178e61460262_Trophy.png",
+                },
+                "texts": {
                     "title": "1 Step Closer To YourGoal!",
                     "text": "I'm happy for you and hope you keep going.",
                 }
+            },
+            {
+                "type": "AMRAP",
+                "title": "AMRAP",
+                "texts": {
+                    "heading": "AMRAP",
+                    "explanation": "Those 3 exercises form 1 round. Do As Many Rounds As Possible within 7 min.",
+                    "tut-button": "How to do&nbsp;those 3 exercises",
+                },
+                "clock": 10,
+                "images": {
+                    "amrap-workout": "http://uploads.webflow.com/58c062e37f6bd43c050cce7f/58c352892387178e61460262_Trophy.png",
+                },
+                "videos": [
+                    {
+                        "title": "Squats",
+                        "srd": "//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FDy28eq2PjcM%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DDy28eq2PjcM&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FDy28eq2PjcM%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&type=text%2Fhtml&schema=youtube"
+                    },{
+                        "title": "Push-Ups",
+                        "src": "//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FIODxDxX7oi4%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DIODxDxX7oi4&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FIODxDxX7oi4%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&type=text%2Fhtml&schema=youtube"
+                    },{
+                        "title": "Sit-Ups",
+                        "src": "//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2Fy5BpvYGyVb0%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dy5BpvYGyVb0&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fy5BpvYGyVb0%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&type=text%2Fhtml&schema=youtube"
+                    },
+                ],
+            },
+            {
+                "type": "AMRAPtut",
+                "title": "AMRAPtut",
+                "texts": {
+                    "heading": "AMRAP",
+                    "explanation": "Those 3 exercises form 1 round. Do As Many Rounds As Possible within 7 min.",
+                    "tut-button": "How to do&nbsp;those 3 exercises",
+                    "amrap-how-to": "Within 20 min, do as many rounds as you can, consisting of the shown 5 exercises.",
+                },
+                "clock": 10,
+                "images": {
+                    "amrap-workout": "http://uploads.webflow.com/58c062e37f6bd43c050cce7f/58c28dd12a203f5d54fff426_7%20min%20AMRAP%20example.png",
+                },
+                "videos": [
+                    {
+                        "title": "Squats",
+                        "srd": "//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FDy28eq2PjcM%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DDy28eq2PjcM&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FDy28eq2PjcM%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&type=text%2Fhtml&schema=youtube"
+                    },{
+                        "title": "Push-Ups",
+                        "src": "//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FIODxDxX7oi4%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DIODxDxX7oi4&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FIODxDxX7oi4%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&type=text%2Fhtml&schema=youtube"
+                    },{
+                        "title": "Sit-Ups",
+                        "src": "//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2Fy5BpvYGyVb0%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dy5BpvYGyVb0&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fy5BpvYGyVb0%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&type=text%2Fhtml&schema=youtube"
+                    },
+                ],
+            },
+            {
+                "type": "Stretching",
+                "title": "Stretching",
+                "texts": {
+                    "explanation": "Take your time. Breathe all the time. Don't spoil yourself, but don't stretch to agony.",
+                },
+                "clock": 10,
+                "images": {
+                    "pic": "http://uploads.webflow.com/58c062e37f6bd43c050cce7f/58c4617357a2703f67303098_ShouldersWeb.png",
+                },
+            },
+            {
+                "type": "Warmup",
+                "title": "Warmup",
+                "texts": {
+                    "heading": "Warmup",
+                    "explanation": "Don't go too fast, but I want to see your pulse high after. Let's go!",
+                },
+                "clock": 10,
+                "images": {
+                    "pic": "http://uploads.webflow.com/58c062e37f6bd43c050cce7f/58c4617357a2703f67303098_ShouldersWeb.png",
+                },
+            },
+            {
+                "type": "Welcome",
+                "title": "Welcome",
+                "texts": {
+                    "title": "Welcome to your home workout!",
+                    "text": "You're going to warm up doing cardio, then do a stretching routine twice, and lastly finish it off with a challenging workout.",
+                },
             },
         ]
     },
