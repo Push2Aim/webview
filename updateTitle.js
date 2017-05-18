88203ee345b91b6e5fc2dca787357c4ee3a62828
push2aim <jon@Johanness-MBP.local>
2017-05-18 19:44:42 +0200

    Updates

--- updateTitle.js
+++
@@ -1,8 +0,0 @@
-var Webflow = Webflow || [];
-Webflow.push(function() {
-    $('.heads').children().on('click', function(e) {
-        var index = $('.heads .w-inline-block').index(this);
-        console.log(index);
-        $(".slider-navigation-dots").children().eq(index).trigger('tap');
-    })
-});
